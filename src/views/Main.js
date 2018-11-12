df6336d8ab4c7d450fe5124cae85867ec7e12bc9
Leonardo Couy <contato@leonardocouy.com>
2018-11-12 11:43:13 -0200

    Fix list margin bottom behavior

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import ThreadsContainer from '../containers/ThreadsContainer';
 
+const MainContainer = styled.main`
+  margin-bottom: 64px;
+`
+
 const Main = () => (
-  <main className="main-container">
+  <MainContainer>
     <ThreadsContainer />
-  </main>
+  </MainContainer>
 )
 
 export default Main;
