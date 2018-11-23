1a73d7d381f1c8f6ee58fb8d10e29a83bc6c0586
Leonardo Couy <contato@leonardocouy.com>
2018-11-23 14:54:49 -0200

    Remove favicon

--- src/App.js
+++ src/App.js
@@ -28,14 +28,6 @@ class App extends Component {
         <Navbar  />
         <Main />
         <Footer />
-        <GoogleTagManager gtmId={process.env.REACT_APP_GOOGLE_TGM_KEY} />
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
-          { (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: process.env.REACT_APP_GOOGLE_ADS_KEY,
-            enable_page_level_ads: true
-          }) };
-        </script>
       </div>
     );
   }
